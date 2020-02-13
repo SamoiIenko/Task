a4e977724a0c38e63c5d4ea7306d7bb22022e375
SamoiIenko <samoilenkoand@gmail.com>
2020-02-13 18:56:40 +0300

    Revert "Update main.js"
    This reverts commit 63a19288f5104d006bd4928bf98c3f6d4b142e02.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
+// Описание задачи
+// 1. Починить код
+// 2. Добавить кнопку и функцию удаления задачи
+// 3. Посмотреть на код и
+// 3.1. Перечислить, что бы вы сделали по-другому
+// 3.2. [опционально] Переписать код как душа просит
+// Комментарии по ошибкам можно писать прямо в коде
 
-import vue from "vue"; 
+
+// Для удобства изменен v-on:click на @click
+import vue from "vue"; // Изменил require на import
 
 window.app = new vue({
   el: "#app",
@@ -13,13 +22,13 @@ window.app = new vue({
       value: ""
     };
   },
-  mounted() { 
+  mounted() { // Была изменена функция из created на mounted
     var search = document.getElementById("search") || {};
     search.focus();
   },
   template: `
     <div>
-        <input v-model="value" id="search" /> 
+        <input v-model="value" id="search" /> <!-- v-bind изменен на v-model -->
         <button @click="todo(value)">Добавить задачу</button>
         <div v-if="innerData.activeFilter == 'active'">
           <div v-for="(todo, index) in innerData.zadachi" v-if="todo.completed != true">
@@ -56,15 +65,15 @@ window.app = new vue({
         name: t,
         completed: false
       };
-    
+     // innerData.zadachi[innerData.zadachi.length + 1] = t; 
       this.innerData.zadachi.push(add);
     },
-    remove(index) {
+    remove(index) { // Активную задачу можно отправить в завершенную 
       var copy = this.innerData.zadachi.slice();
       copy[index].completed = true;
       this.$set(this.innerData, "zadachi", copy);
     },
-    deleteTodo(index) { 
+    deleteTodo(index) { // Добавлена функция удаления задачи из завершенных задач
       var copy = this.innerData.zadachi.slice();
       copy.splice(index,1);
       this.$set(this.innerData, "zadachi", copy);
